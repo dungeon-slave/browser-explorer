26246f7493557034f1c84a5af14c13e02e7faef9
snus <vasilevsky.matvey@gmail.com>
2023-05-01 11:47:09 +0300

    remove useEffect because it dont need

--- subComponents/ExplorerStructure.tsx
+++ subComponents/ExplorerStructure.tsx
@@ -1,18 +1,16 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Data } from "../scripts/Data";
 
 function ExplorerStructure(props : {data: Data})
 {
-    useEffect(() => {}, [props.data]);
-
     return(
         <div className="ExplorerStructure">
             <>
                 <ul>
-                {props.data.pathes.map(path =>
-                    <li key={path}>
-                        ${path}
-                    </li>)}
+                    {props.data.pathes.map(path =>
+                        <li key={path}>
+                            ${path}
+                        </li>)}
                 </ul>
             </>
         </div>
