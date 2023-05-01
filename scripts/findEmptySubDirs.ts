3aac0db13bf9237ac20ade2b5daa7bb8493ca877
snus <vasilevsky.matvey@gmail.com>
2023-05-01 11:28:34 +0300

    add stub to future function

---
+++ scripts/findEmptySubDirs.ts
@@ -0,0 +1,11 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+function findEmptySubDirs(parentDirPath: string) : string[]
+{
+    let subDirsPathes : string[] = [];
+    
+
+
+    return subDirsPathes;
+}
