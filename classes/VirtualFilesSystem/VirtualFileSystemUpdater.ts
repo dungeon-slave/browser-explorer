6781c034944108753c7cd3ce8ebdf32a6ef546d4
snus <vasilevsky.matvey@gmail.com>
2023-05-10 02:53:09 +0300

    make prototype to updater

---
+++ classes/VirtualFilesSystem/VirtualFileSystemUpdater.ts
@@ -0,0 +1,39 @@
+import VirtualFileSystemInstance from "./VirtualFileSystemInstance";
+
+export class VirtualFileSystemUpdater extends VirtualFileSystemInstance
+{
+    public renameDirectory()
+    {
+
+    }
+
+    public renameFile()
+    {
+
+    }
+
+    public addDirectory()
+    {
+
+    }
+
+    public removeDirectory()
+    {
+
+    }
+
+    public addFile()
+    {
+
+    }
+
+    public removeFile()
+    {
+
+    }
+
+    public changeFileText()
+    {
+
+    }
+}
