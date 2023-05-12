22ee06eadedf99cbf118e030d8f2a62f69fd1629
snus <vasilevsky.matvey@gmail.com>
2023-05-13 02:51:48 +0300

    update stub

--- classes/VirtualFilesSystem/VirtualFileSystemUpdater.ts
+++ classes/VirtualFilesSystem/VirtualFileSystemUpdater.ts
@@ -1,3 +1,5 @@
+import Directory from "../FileSystemElements/Directory";
+import File from "../FileSystemElements/File";
 import VirtualFileSystemInstance from "./VirtualFileSystemInstance";
 
 export class VirtualFileSystemUpdater extends VirtualFileSystemInstance
@@ -22,9 +24,28 @@ export class VirtualFileSystemUpdater extends VirtualFileSystemInstance
 
     }
 
-    public addFile()
+    public static addFile(parentFolderPath : string, fileName : string)
     {
+        let foldersWay : string[] = parentFolderPath.split('/').filter(item => item !== VirtualFileSystemInstance.root.name);
+        let currDirectory : Directory = VirtualFileSystemInstance.root;
 
+        while (foldersWay.length > 0) 
+        {
+            const nextDirectory = currDirectory.subDirectories.find((element) => 
+            {
+                if (element !== undefined) 
+                {
+                    return element.name === foldersWay[0];
+                }
+            });
+            if (nextDirectory !== undefined) 
+            {
+                currDirectory = nextDirectory; 
+            }
+            foldersWay.shift();
+        }
+
+        currDirectory.addFile(new File(fileName, "", parentFolderPath + '/' + fileName));
     }
 
     public removeFile()
