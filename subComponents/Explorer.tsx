3cafbec16b2c6d4e7cd85347935b91a66595a6a4
snus <vasilevsky.matvey@gmail.com>
2023-05-13 02:51:13 +0300

    remove unnecessary async

--- subComponents/Explorer.tsx
+++ subComponents/Explorer.tsx
@@ -1,23 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import React from "react";
 import Inputs from "./Inputs";
-import { RootBuilder } from "../classes/VirtualFilesSystem/RootBuilder";
 import ExplorerStructure from "./ExplorerStructureElements/ExplorerStructure";
 
 function Explorer() 
 {
 	const [structureState, setStructureState] = useState<boolean>(false);
-
-	async function updateRoot() 
-	{
-		await RootBuilder.updateRoot();
-	}
-
-	useEffect(() => 
-	{
-		updateRoot().then(() => setStructureState(true));
-	}, []);
-
 
 	return (
 		<div className="Explorer">
